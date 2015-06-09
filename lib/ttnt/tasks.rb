1e3ef80d8bbafae6eda41a8d71fe90409b78c9cd
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-09 16:05:03 +0900

    Define ttnt:run rake task

--- lib/ttnt/tasks.rb
+++ lib/ttnt/tasks.rb
@@ -1,6 +1,8 @@
 require 'rake/testtask'
 require 'rake/file_list'
 require 'ttnt/testtask'
+require 'ttnt/test_selector'
+require 'rugged'
 
 # https://github.com/bundler/bundler/blob/master/lib/bundler/gem_helper.rb
 module TTNT
@@ -27,6 +29,21 @@ module TTNT
           args += " -I#{test_task.libs.join(':')}" unless test_task.libs.empty?
           test_files.each do |test_file|
             ruby "#{args} #{test_file}"
+          end
+        end
+
+        desc 'Run only tests related to changes in TARGET_SHA (defaults to HEAD) against BASE_SHA (defaults to merge base against master)'
+        task 'test' do
+          repo = Rugged::Repository.discover('.')
+          target_sha = ENV['TARGET_SHA'] || repo.head.target_id
+          base_sha = ENV['BASE_SHA'] || repo.merge_base(target_sha, repo.rev_parse('master'))
+          ts = TTNT::TestSelector.new(target_sha, base_sha)
+          tests = ts.select_tests
+          if tests.empty?
+            STDERR.puts 'No test selected.'
+          else
+            # TODO: actually run tests
+            puts tests
           end
         end
       end
