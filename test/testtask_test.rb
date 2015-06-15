5780582d843fc37f8ee521fba83ec51f88bf9bb8
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-15 11:26:34 +0900

    Make TTNT::TsetTask define rake tasks to match Rake::TestTask behavior

--- test/testtask_test.rb
+++ test/testtask_test.rb
@@ -3,18 +3,28 @@ require 'ttnt/testtask'
 require 'rake/testtask'
 
 class TestTaskTest < Minitest::Test
-  def test_attributes
-    rake_task = nil
+  def setup
+    @name = 'sample_name'
+    @rake_task = nil
     # This will be in users' Rakefiles
-    Rake::TestTask.new { |t|
+    @ttnt_task = Rake::TestTask.new { |t|
+      t.name = @name
       t.libs << 'test'
       t.pattern = 'test/**/*_test.rb'
       TTNT::TestTask.new(t)
-      rake_task = t # save for testing purpose
+      @rake_task = t # save for testing purpose
     }
+  end
 
-    ttnt_task = TTNT::TestTask.instances.first
-    assert_equal rake_task.libs, ttnt_task.libs
-    assert_equal rake_task.pattern, ttnt_task.pattern
+  def test_define_rake_tasks
+    assert Rake::Task.task_defined?("ttnt:#{@name}:anchor"),
+      "`ttnt:#{@name}:anchor` task should be defined"
+    assert Rake::Task.task_defined?("ttnt:#{@name}:run"),
+      "`ttnt:#{@name}:run` task should be defined"
+  end
+
+  def test_attributes
+    assert_equal @rake_task.libs, @ttnt_task.libs
+    assert_equal @rake_task.pattern, @ttnt_task.pattern
   end
 end
