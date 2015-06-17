bd78dc3dc60b1b058107740e889b7e4bc6c5d9b6
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-17 17:30:07 +0900

    Add documents to TTNT module and VERSION constant to make coverage 100%

--- lib/ttnt.rb
+++ lib/ttnt.rb
@@ -1,5 +1,8 @@
 require "ttnt/version"
 
+# Test This, Not That!
+#
+# See {file:README.md} for more detail.
 module TTNT
   # Your code goes here...
 end
--- lib/ttnt/version.rb
+++ lib/ttnt/version.rb
@@ -1,3 +1,4 @@
 module TTNT
+  # TTNT version
   VERSION = "0.0.2"
 end
