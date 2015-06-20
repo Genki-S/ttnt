98249ac664337bf2c8bd8efb79508fcac8d98cd8
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-20 14:09:41 +0900

    Write documents for TTNT::TestTask

--- lib/ttnt/testtask.rb
+++ lib/ttnt/testtask.rb
@@ -3,9 +3,15 @@ require 'rake'
 require 'ttnt/test_selector'
 
 module TTNT
+  # TTNT version of Rake::TestTask.
+  # Uses configuration from Rake::TestTask to minimize user configuration.
+  # Defines TTNT related rake tasks when instantiated.
   class TestTask
     include Rake::DSL
 
+    # Create an instance of TTNT::TestTask and define TTNT rake tasks.
+    #
+    # @param rake_test_task [Rake::TestTask] an instance of Rake::TestTask after user configuration is done
     def initialize(rake_test_task)
       attributes = rake_test_task.instance_variables
       attributes.map! { |attribute| attribute[1..-1] }
@@ -24,13 +30,19 @@ module TTNT
       define_tasks
     end
 
+    # Git repository discovered from current directory
+    #
+    # @return [Rugged::Reposiotry]
     def repo
       @repo ||= Rugged::Repository.discover('.')
     end
 
-    # Task definitions are taken from Rake::TestTask
-    # https://github.com/ruby/rake/blob/e644af3/lib/rake/testtask.rb#L98-L112
+    # Define TTNT tasks under namespace 'ttnt:TESTNAME'
+    #
+    # @return [void]
     def define_tasks
+      # Task definitions are taken from Rake::TestTask
+      # https://github.com/ruby/rake/blob/e644af3/lib/rake/testtask.rb#L98-L112
       namespace :ttnt do
         namespace @name do
           define_run_task
@@ -39,6 +51,13 @@ module TTNT
       end
     end
 
+    # Define a task which runs only tests which might have affected from changes
+    # in BASE_SHA...TARGET_SHA
+    #
+    # TARGET_SHA and BASE_SHA can be specified as an environment variable. They
+    # defaults to HEAD and merge base between master and TARGET_SHA, respectively.
+    #
+    # @return [void]
     def define_run_task
       desc @run_description
       task 'run' do
@@ -57,6 +76,10 @@ module TTNT
       end
     end
 
+    # Define a task which runs test files file by file, and generate and save
+    # test-to-code mapping.
+    #
+    # @return [void]
     def define_anchor_task
       desc @anchor_description
       task 'anchor' do
