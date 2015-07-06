c2909fdcc93032760837503dc7e91b6c9c56dade
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-07-06 17:57:33 +0900

    Make TTNT::Storage read storage file content from git history

--- lib/ttnt/storage.rb
+++ lib/ttnt/storage.rb
@@ -1,8 +1,17 @@
 module TTNT
   # A storage to store TTNT data such as test-to-code mapping and metadata.
   class Storage
-    def initialize(repo)
+    # Initialize the storage from given repo and sha. This reads contents from
+    # a file `.ttnt`. When sha is not nil, contents of the file on that commit
+    # is read. Data can be written only when sha is nil (written to current
+    # working tree).
+    #
+    # @param repo [Rugged::Repository]
+    # @param sha [String] sha of the commit from which data should be read
+    #   nil means reading from/writing to current working tree.
+    def initialize(repo, sha = nil)
       @repo = repo
+      @sha = sha
     end
 
     # Read data in section from the storage.
@@ -10,8 +19,9 @@ module TTNT
     # @param section [String]
     # @return [Hash]
     def read(section)
-      if File.size?(filename)
-        JSON.parse(File.read(filename))[section] || {}
+      str = read_storage_content
+      if str.length > 0
+        JSON.parse(str)[section] || {}
       else
         {}
       end
@@ -23,6 +33,7 @@ module TTNT
     # @param section [String]
     # @param value [Hash]
     def write!(section, value)
+      raise 'Data cannot be written to the storage back in git history' unless @sha.nil?
       File.open(filename, File::RDWR|File::CREAT, 0644) do |f|
         f.flock(File::LOCK_EX)
         str = f.read
@@ -39,6 +50,18 @@ module TTNT
 
     def filename
       "#{@repo.workdir}/.ttnt"
+    end
+
+    def read_storage_content
+      if @sha
+        @repo.lookup(@repo.lookup(@sha).tree['.ttnt'][:oid]).content
+      else
+        if File.exist?(filename)
+          File.read(filename)
+        else
+          ""
+        end
+      end
     end
   end
 end
--- test/storage_test.rb
+++ test/storage_test.rb
@@ -16,9 +16,25 @@ class StorageTest < TTNT::TestCase
     assert_equal @data, @storage.read(@section)
   end
 
+  def test_read_storage_from_history
+    @storage.write!(@section, @data)
+    git_commit_am('Add data to storage file')
+    sha = @repo.head.target_id
+    new_data = { 'c' => 3 }
+    @storage.write!(@section, new_data) # write to a file in working tree
+    history_storage = TTNT::Storage.new(@repo, sha)
+    assert_equal @data, history_storage.read(@section)
+  end
+
   def test_write_storage
     @storage.write!(@section, @data)
     assert File.exist?(@storage_file), 'Storage file should be created.'
     assert_equal @data, JSON.parse(File.read(@storage_file))[@section]
+  end
+
+  def test_cannot_write_to_history_storage
+    sha = @repo.head.target_id
+    history_storage = TTNT::Storage.new(@repo, sha)
+    assert_raises { history_storage.write!(@section, @data) }
   end
 end
