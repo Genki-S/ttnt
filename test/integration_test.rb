f425418c27f09dd00df69feb6aed6c2ce9dc17ab
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-08-05 14:58:11 +0900

    Add integration test to test `.ttnt` file is Rakefile binded

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -76,6 +76,31 @@ module TTNT
         output = rake('ttnt:test:run')
         assert_match '1 runs, 3 assertions, 0 failures', output[:stdout]
       end
+
+      def test_storage_file_resides_with_rakefile
+        tmpdir = 'tmp'
+        Dir.mkdir(tmpdir)
+        git_rm_and_commit("#{@repo.workdir}/.ttnt", 'Remove .ttnt file')
+        File.delete("#{@repo.workdir}/.ttnt")
+        %w(fizzbuzz.rb fizz_test.rb buzz_test.rb Rakefile).each do |file|
+          FileUtils.mv file, tmpdir
+          git_rm_and_commit(file, "Remove #{file}")
+        end
+        git_commit_am("Move files into #{tmpdir}")
+
+        Dir.chdir(tmpdir)
+        load_rakefile("#{Dir.pwd}/Rakefile")
+
+        # Test writing to storage
+        rake('ttnt:test:anchor')
+        assert File.exist?("#{@repo.workdir}/#{tmpdir}/.ttnt")
+        assert !File.exist?("#{@repo.workdir}/.ttnt")
+        git_commit_am('Add new .ttnt file under tmp directory')
+
+        # Test reading from storage
+        output = rake('ttnt:test:run')
+        assert_match 'No test selected.', output[:stderr]
+      end
     end
 
     class AdditionAmongComments < TTNT::TestCase::AdditionAmongComments
