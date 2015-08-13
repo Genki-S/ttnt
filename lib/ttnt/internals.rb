8d226b0acde3271b6ac712c3dae6c5c7e77c9b04
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-08-13 10:21:49 +0900

    Use class variable rather than instance variable

--- lib/ttnt/internals.rb
+++ lib/ttnt/internals.rb
@@ -3,11 +3,11 @@ require 'rake'
 module TTNT
   class << self
     def root_dir
-      @root_dir ||= Rake.application.find_rakefile_location[1]
+      @@root_dir ||= Rake.application.find_rakefile_location[1]
     end
 
     def root_dir=(dir)
-      @root_dir = dir
+      @@root_dir = dir
     end
   end
 end
